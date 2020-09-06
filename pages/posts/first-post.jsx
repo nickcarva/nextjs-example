5b4d8caf7bc93eed8b1bf3c7b73ab8345223760d
nickcarva <carvalhonick2002@gmail.com>
2020-09-06 11:45:21 -0300

    Adding some styles and image to the project

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
+import Layout from '../../components/Layout'
 
 function FirstPost () {
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Fist Post Page</title>
       </Head>
 
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </div>
+      <h1>Lorem Ipsum</h1>
+      <h4>
+        Issued at 09/06/2020 11:41 AM<br/>
+        223 views
+      </h4>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus turpis in eu mi bibendum. Pretium quam vulputate dignissim suspendisse in est. Dignissim sodales ut eu sem integer. Dolor purus non enim praesent elementum. Sem nulla pharetra diam sit amet nisl suscipit. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Amet mauris commodo quis imperdiet massa. In massa tempor nec feugiat nisl. At lectus urna duis convallis convallis. Varius morbi enim nunc faucibus a pellentesque sit. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin. Tincidunt ornare massa eget egestas.
+      </p>
+
+      <p>
+        Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Nisl tincidunt eget nullam non nisi est. Arcu dui vivamus arcu felis bibendum ut. Porttitor leo a diam sollicitudin tempor. Aliquam ultrices sagittis orci a scelerisque. Euismod in pellentesque massa placerat duis ultricies. Amet consectetur adipiscing elit ut aliquam purus sit amet. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Quam id leo in vitae turpis massa sed elementum. Aliquet nec ullamcorper sit amet. Dignissim cras tincidunt lobortis feugiat vivamus at. Eros donec ac odio tempor orci dapibus. Mauris rhoncus aenean vel elit scelerisque. Nunc mattis enim ut tellus. Adipiscing elit ut aliquam purus sit amet luctus venenatis. Arcu risus quis varius quam quisque id diam. A lacus vestibulum sed arcu non odio. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Dictum non consectetur a erat.
+      </p>
+
+      <p>
+        Est ante in nibh mauris cursus mattis molestie. Neque convallis a cras semper auctor neque vitae tempus. Sed faucibus turpis in eu mi. Nec sagittis aliquam malesuada bibendum arcu vitae. Lobortis scelerisque fermentum dui faucibus. Sit amet mattis vulputate enim nulla aliquet. Diam quis enim lobortis scelerisque fermentum dui. Ridiculus mus mauris vitae ultricies leo integer malesuada. Est ullamcorper eget nulla facilisi etiam. Amet purus gravida quis blandit turpis cursus. Amet nulla facilisi morbi tempus iaculis urna id volutpat. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Quis imperdiet massa tincidunt nunc pulvinar sapien. Vitae elementum curabitur vitae nunc. Erat imperdiet sed euismod nisi porta lorem mollis.
+      </p>
+
+      <p>
+        Auctor eu augue ut lectus arcu. Nec nam aliquam sem et tortor consequat. Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Arcu vitae elementum curabitur vitae. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Cum sociis natoque penatibus et magnis dis parturient montes. Nullam eget felis eget nunc lobortis. Fringilla est ullamcorper eget nulla facilisi. Ornare lectus sit amet est placerat in. Eget nullam non nisi est sit amet facilisis magna etiam. Congue nisi vitae suscipit tellus mauris a diam. Nunc aliquet bibendum enim facilisis gravida neque.
+      </p>
+
+      <p>
+        Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Fermentum posuere urna nec tincidunt praesent semper. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Massa vitae tortor condimentum lacinia quis vel eros donec ac. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Posuere morbi leo urna molestie at. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Blandit turpis cursus in hac. Viverra suspendisse potenti nullam ac tortor vitae purus. Magna fermentum iaculis eu non. Magnis dis parturient montes nascetur ridiculus. Id semper risus in hendrerit gravida rutrum. Risus pretium quam vulputate dignissim suspendisse in. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam.
+      </p>
+    </Layout>
   )
 }
 
