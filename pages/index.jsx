1f0bd58b8505847590c101dfecba07d1bb227154
nickcarva <carvalhonick2002@gmail.com>
2020-09-07 14:12:33 -0300

    Getting posts from markdown files

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,21 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 import Layout, { siteTitle } from '../components/Layout'
+import { getSortedPostsData } from '../lib/posts'
+
 import utilStyles from '../styles/utils.module.css'
 
-function Home () {
+export async function getStaticProps () {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+// eslint-disable-next-line react/prop-types
+function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -26,6 +38,20 @@ function Home () {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
